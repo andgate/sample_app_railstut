32b5400a2dab3748ea74c2ac8a056adbf1af0986
Gabriel Anderson <zettablade@gmail.com>
2016-03-17 11:27:55 +0000

    Add integration test for invalid user login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Login and redirect to user's show page
     else
-      # create an error message
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
